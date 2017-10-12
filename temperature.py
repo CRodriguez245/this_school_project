66a5b2d4c84fac072fc9c7c3f363489646d7b8a8
rsydorchuk <32551225+rsydorchuk@users.noreply.github.com>
2017-10-11 20:20:41 -0500

    Create temperature.py

---
+++ temperature.py
@@ -0,0 +1,39 @@
+F = 0
+C = 1
+K = 2
+
+print("Please enter your conversion in this format: lowercase_unit_from(value,uppercase_unit_to)")
+print("")
+print("Example:")
+print("To convert 45 degrees Fahrenheit to degrees Celsius, type:")
+print("f(45,C)")
+
+def f(value, base):
+    if base == F:
+        return value
+    elif base == C:
+        return (value - 32)/1.8
+    elif base == K:
+        return (value - 32)/1.8 + 273.15
+    else:
+        print("Error: incorrect format.")
+
+def c(value, base):
+    if base == C:
+        return value
+    elif base == F:
+        return (1.8 + value) + 32
+    elif base == K:
+        return value + 273.15
+    else:
+        print("Error: incorrect format.")
+
+def k(value, base):
+    if base == K:
+        return value
+    elif base == C:
+        return (value - 273.15)
+    elif base == F:
+        return (value - 273.15) * 1.8 + 32
+    else:
+        print("Error: incorrect format.")
